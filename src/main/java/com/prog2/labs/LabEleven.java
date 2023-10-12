e265e066e8eb878dffd7529db8cee7582964ed8b
HSterben <142906656+HSterben@users.noreply.github.com>
2023-10-12 12:47:45 -0400

    Update LabEleven.java
    Finished

--- src/main/java/com/prog2/labs/LabEleven.java
+++ src/main/java/com/prog2/labs/LabEleven.java
@@ -10,24 +10,50 @@ import java.util.Map;
  * @author adinashby
  *
  */
-
 public class LabEleven {
 
-	/**
-	 * Write your test code below in the main (optional).
-	 *
-	 */
-	public static void main(String[] args) {
+    /**
+     * Write your test code below in the main (optional).
+     *
+     */
+    public static void main(String[] args) {
 
-	}
+    }
 
-	/**
-	 * Please refer to the README file for question(s) description
-	 */
-
+    /**
+     * Please refer to the README file for question(s) description
+     */
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return new ArrayList();
-        
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            for (int i = 0; i < charArray.length; i++) {
+                for (int j = 0; j < charArray.length; j++) {
+                    if (charArray[i] > charArray[j]) {
+                        char temp = charArray[i];
+                        charArray[i] = charArray[j];
+                        charArray[j] = temp;
+                    }
+                }
+            }
+            String key = new String(charArray);
+            if (map.containsKey(key)) {
+                map.get(key).add(s);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(key, list);
+            }
+        }
+
+        List<List<String>> answer = new ArrayList<>();
+
+        for (List i : map.values()) {
+            answer.add(i);
+        }
+        return answer;
+
     }
 
 }
